5aa9d6f7fe0ac8a9c69b4b441a361856f8d16bc4
hyewon218 <won_11111@naver.com>
2023-06-22 16:18:21 +0900

    @ManyToOne 단방향 테스트

--- src/main/java/com/sparta/jpaadvance/entity/User.java
+++ src/main/java/com/sparta/jpaadvance/entity/User.java
@@ -13,14 +13,4 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-
-    @OneToOne(mappedBy = "user")
-    private Food food;
-
-    public void addFood(Food food) {
-        this.food = food;
-        // User 객체 자신을 실제 외래키의 주인에게 주게 되는 것
-        // 외래키 설정 (필드로 외래키의 주인을 받아오기 때문에 this 를 넘겨줌)
-        food.setUser(this);
-    }
 }
